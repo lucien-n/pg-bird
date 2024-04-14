e4f54fdfb7e9d2fe49664818c601a4b0e534ee0a
Lucien <lucien.neuhoff@protonmail.com>
2024-04-14 21:02:15 +0200

    feat: :sparkles: start screen animation

--- src/hud.py
+++ src/hud.py
@@ -1,4 +1,5 @@
 from .settings import *
+import math
 
 
 class Hud:
@@ -9,6 +10,10 @@ class Hud:
 
         self.digits = self.load_numbers()
         self.gameover = pg.image.load(path / "assets/gameover.png").convert_alpha()
+
+        self.start_message = pg.image.load(path / "assets/message.png").convert_alpha()
+        self.start_message_scale = 1
+        self.start_message_scale_direction = 1
 
     def load_numbers(self) -> list[pg.sprite.Sprite]:
         numbers = []
@@ -27,7 +32,7 @@ class Hud:
 
         return score_surfaces
 
-    def draw(self, surface: pg.Surface):
+    def draw_ingame(self, surface: pg.Surface):
         padding = 8
         score_surfaces = self.get_score_surfaces(self.game.score)
 
@@ -45,3 +50,26 @@ class Hud:
             x = surface.get_width() / 2 - self.gameover.get_width() / 2
             y = surface.get_height() / 2 - self.gameover.get_height() / 2
             surface.blit(self.gameover, (x, y))
+
+    def draw_start(self, surface: pg.Surface):
+        if self.start_message_scale > 1.3:
+            self.start_message_scale_direction = -1
+        elif self.start_message_scale < 1:
+            self.start_message_scale_direction = 1
+
+        self.start_message_scale = math.sin(time() * 12) / 35 + 1
+
+        scaled_width = self.start_message.get_width() * self.start_message_scale
+        scaled_height = self.start_message.get_height() * self.start_message_scale
+
+        scaled = pg.transform.scale(self.start_message, (scaled_width, scaled_height))
+
+        x = GAME_WIDTH / 2 - scaled_width / 2
+        y = GAME_HEIGHT / 2 - scaled_height / 2
+        surface.blit(scaled, (x, y))
+
+    def draw(self, surface: pg.Surface):
+        if self.game.playing:
+            self.draw_ingame(surface)
+        else:
+            self.draw_start(surface)
