a2ad00eb1d6e5305c1d313aaf67eeeda07c63a5e
Lucien <lucien.neuhoff@protonmail.com>
2024-04-14 17:53:58 +0200

    feat: :sparkles: pipe sprites

--- src/piper.py
+++ src/piper.py
@@ -10,6 +10,18 @@ class Piper:
         self.pipes: list[Pipe] = []
         self.passed_pipes = 0
 
+        self.top_pipe, self.bottom_pipe = self.load_pipes("green")
+        self.pipe_height = self.top_pipe.get_height()
+
+    def load_pipes(self, color: str):
+        if not color == "red" and not color == "green":
+            raise Exception(f"Pipe '{color}' not found")
+
+        pipe_bottom = pg.image.load(path / rf"assets/pipe-{color}.png").convert_alpha()
+        pipe_top = pg.transform.flip(pipe_bottom, True, True)
+
+        return (pipe_top, pipe_bottom)
+
     def update(self, dt: float):
         # spawn a new pipe of the last spawned pipe traveled a third of the canvas
         last_pipe = self.pipes[-1] if len(self.pipes) > 0 else None
@@ -32,4 +44,9 @@ class Piper:
         [pipe.update(dt) for pipe in self.pipes]
 
     def draw(self, surface: pg.Surface):
-        [pipe.draw(surface) for pipe in self.pipes]
+        for pipe in self.pipes:
+            # top
+            surface.blit(self.top_pipe, (pipe.x, -self.pipe_height + pipe.gap_start))
+
+            # bottom
+            surface.blit(self.bottom_pipe, (pipe.x, pipe.gap_end))
