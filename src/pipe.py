e8baa0ac5dd9528258564ee9a7c60c6cbd6f389e
Lucien <lucien.neuhoff@protonmail.com>
2024-04-14 17:54:43 +0200

    refactor: :wastebasket: removed pipe draw method

--- src/pipe.py
+++ src/pipe.py
@@ -15,12 +15,3 @@ class Pipe:
 
     def update(self, dt: float):
         self.x -= 150 * dt
-
-    def draw(self, surface: pg.Surface):
-        top_rect = pg.Rect(self.x, 0, PIPE_WIDTH, self.gap_start)
-        pg.draw.rect(surface, Colors.PIPE, top_rect)
-
-        bottom_rect = pg.Rect(
-            self.x, self.gap_end, PIPE_WIDTH, CANVAS_SIZE - self.gap_end
-        )
-        pg.draw.rect(surface, Colors.PIPE, bottom_rect)
